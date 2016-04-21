4c64fe851fea765fdbdc3327bd8cada723b7ceb7
josedes@gmail.com <asoto999@hotmail.com>
2016-04-21 05:45:02 -0430

    Creacion de la pagina principal dinamica fecha de cumpleados

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,12 +48,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.assets.debug = true
  
-   config.action_mailer.smtp_settings = { 
+  config.action_mailer.smtp_settings = { 
   address: "smtp.gmail.com",
   port: 587,
   domain: ENV["DOMAIN_NAME"],
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  password: ENV["GMAIL_PASSWORD"]}
 end
